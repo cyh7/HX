a97522f87aac7c2b94f333275784775ea1ced212
cyh7 <55831382+cyh7@users.noreply.github.com>
2021-01-30 09:22:26 +0800

    修改卓承的cad路径，并添加cad按钮标志位

--- HX/CcadDlg.h
+++ HX/CcadDlg.h
@@ -2,9 +2,10 @@
 #include "CmodbusDlg.h"
 #include "CLayout.h"
 #include "CMyButton.h"
+#include "PathGen.h"
 
-//cad按钮状态标志位
 extern bool CadBtnStatus;
+
 // CcadDlg 对话框
 
 class CcadDlg : public CDialogEx, public has_slots<>
@@ -15,7 +16,7 @@ public:
 	CcadDlg(CWnd* pParent = nullptr);   // 标准构造函数
 	virtual ~CcadDlg();
 
-// 对话框数据
+	// 对话框数据
 #ifdef AFX_DESIGN_TIME
 	enum { IDD = IDD_CAD };
 #endif
@@ -36,11 +37,11 @@ public:
 	afx_msg BOOL OnHelpInfo(HELPINFO* pHelpInfo);
 	BOOL PreTranslateMessage(MSG* pMsg);
 	CBrush m_Brush;
-//通信功能
+	//通信功能
 public:
 	unsigned short CRC16(unsigned char* puchMsg, unsigned short usDataLen);
 	void SendData(int CommTypeIn, WORD DownAdd, DWORD DownData);
-//按钮重绘
+	//按钮重绘
 public:
 	CMyButton m_cad_btn_open;
 	CMyButton m_cad_btn_draw;
@@ -57,7 +58,7 @@ public:
 	CMyButton m_cad_btn_opdata;
 	CMyButton m_cad_btn_opmod;
 	CMyButton m_cad_btn_opcad;
-//logo
+	//logo
 public:
 	CStatic m_cad_pic_logo;
 	HBITMAP m_cad_hBitmap_logo;
@@ -66,5 +67,30 @@ public:
 	CFont f_cad_name;
 	void BanBtnSend();
 	void EnableBtnSend();
-	static CcadDlg *pCaddlg;
+	static CcadDlg* pCaddlg;
+
+
+
+	///////////////////////////////////////////////////////////CAD画图相关
+	actual_path actual_path_;
+	//笔画颜色，大小
+	Gdiplus::ARGB pen_color_;
+	float pen_width_;
+	//gdi计算机图形类，用于渲染模型
+	shared_ptr<Gdiplus::Graphics> graphics_ptr_;
+	//输出pic区域框
+	CRect pic_rect_;
+
+	//画图初始化函数
+	void GraphicInit();
+
+	//功能函数总览
+	//清图,将输出框重置为白色
+	void ClearPic();
+	void DrawLine(const Gdiplus::ARGB& color, const float& width, const Gdiplus::Point& p1, const Gdiplus::Point& p2);
+	void DrawLineOffset(const double& width_max, const double& height_max, const int& x1, const int& y1, const int& x2, const int& y2);
+
+	bool DrawLineAnimated();
+
+
 };
